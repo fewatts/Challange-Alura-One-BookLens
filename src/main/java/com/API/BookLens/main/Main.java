76efec32e98193537db7718f4b24d149c42b3d7d
Fewatts <feralveswatts@gmail.com>
2024-05-14 17:52:53 -0300

    2º
    Getting API response by ID's

--- src/main/java/com/API/BookLens/main/Main.java
+++ src/main/java/com/API/BookLens/main/Main.java
@@ -1,9 +1,11 @@
 package com.API.BookLens.main;
 
+import com.API.BookLens.service.GetBookInfos;
+
 public class Main {
 
     public void menu() {
-       System.out.println("APP main menu c:");
+       System.out.println(GetBookInfos.getData("http://gutendex.com/books/?ids=11,12,13"));
     }
     
 }
