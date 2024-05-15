eb928cccfe8dca9169c95e2530227dd651601379
Fewatts <feralveswatts@gmail.com>
2024-05-15 11:31:25 -0300

    3º
    Menu and methods structure

--- src/main/java/com/API/BookLens/BookLensApplication.java
+++ src/main/java/com/API/BookLens/BookLensApplication.java
@@ -7,12 +7,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.API.BookLens.main.Main;
 
 @SpringBootApplication
-public class BookLensApplication implements CommandLineRunner{
+public class BookLensApplication implements CommandLineRunner {
 
+	/**
+	 * The main method to start the BookLens application.
+	 * 
+	 * @param args Command-line arguments passed to the application
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(BookLensApplication.class, args);
 	}
 
+	/**
+	 * Executes the main logic of the application upon startup.
+	 * It creates an instance of the Main class and invokes its menu method.
+	 * 
+	 * @param args Command-line arguments passed to the application
+	 * @throws Exception If an error occurs during execution
+	 */
 	@Override
 	public void run(String... args) throws Exception {
 		Main main = new Main();
--- src/main/java/com/API/BookLens/main/Main.java
+++ src/main/java/com/API/BookLens/main/Main.java
@@ -1,11 +1,79 @@
 package com.API.BookLens.main;
 
-import com.API.BookLens.service.GetBookInfos;
+import java.util.Scanner;
 
+/**
+ * The Main class is responsible for managing the main menu and invoking appropriate actions based on user input.
+ */
 public class Main {
 
+    private Scanner scan = new Scanner(System.in);
+
+    /**
+     * Displays the main menu and handles user input to perform various actions.
+     */
     public void menu() {
-       System.out.println(GetBookInfos.getData("http://gutendex.com/books/?ids=11,12,13"));
+
+        while (true) {
+
+            int option = -1;
+            
+            System.out.println("""
+                    -----------------------------------------
+                        Choose your number option:
+                        1- Search book by title
+                        2- List registered books
+                        3- List registered authors
+                        4- List alive authors of certain year
+                        5- List books with a certain language
+                        0- Close application
+                    -----------------------------------------
+                    """);
+
+            try{
+                option = Integer.parseInt(scan.nextLine());
+            }catch(NumberFormatException error){
+                System.out.println("Only numbers are allowed in this menu!");
+                System.out.println(error);
+                System.out.println("-----------------------------------------");
+            }
+            
+            switch (option) {
+
+                case 1 -> searchBookByTitle();
+                case 2 -> listRegisteredBooks();
+                case 3 -> listRegisteredAuthors();
+                case 4 -> listAliveAuthorsOfCertainYear();
+                case 5 -> listBooksWithACertainLanguage();
+                case 0 -> {
+                    System.out.println("Closing application...");
+                    return;
+                }
+                default -> System.out.println("Invalid option!");
+            }
+
+        }
+
     }
-    
+
+    private void searchBookByTitle() {
+        System.out.println("Unimplemented method 'searchBookByTittle'");
+    }
+
+    private void listRegisteredBooks() {
+        System.out.println("Unimplemented method 'listRegisteredBooks'");
+    }
+
+    private void listRegisteredAuthors() {
+        System.out.println("Unimplemented method 'listRegisteredAuthors'");
+    }
+
+    private void listAliveAuthorsOfCertainYear() {
+        System.out.println("Unimplemented method 'listAliveAuthorsOfCertainYear'");
+    }
+
+    private void listBooksWithACertainLanguage() {
+        System.out.println("Unimplemented method 'listBooksWithACertainLanguage'");
+    }
+
 }
